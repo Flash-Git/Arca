c5ebf01719e44f07810c56d76884309b375ea986
James Quinn <flash.jaq@gmail.com>
2019-01-05 09:22:38 +0100

    Ready for code

--- client/src/components/main/Landing.js
+++ client/src/components/main/Landing.js
@@ -1,25 +1,6 @@
 import React, { Component } from "react";
-import Web3 from "web3";
 
 class Landing extends Component {
-    // console.log("cunt");
-    // if(window.ethereum) { //Modern DApp Browsers
-    //   web3 = new Web3(window.ethereum);
-    //   try { 
-    //     window.ethereum.enable().then(
-    //       function() {
-    //         console.log("Web3 Enabled");
-    //       }
-    //     );
-    //   } catch(e) {
-    //     console.log(e);
-    //   }
-    // } else if(window.web3) { //Legacy DApp Browsers
-    //   web3 = new Web3(web3.currentProvider);
-    //   console.log("Degacy web3 enabled");
-    // } else { //Non-DApp Browsers
-    //   alert('Please install MetaMask');
-    // }
   render(){
     return(
       <div id="section-landing">
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import Web3 from "web3";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+let web3;
+if(window.ethereum) { //Modern DApp Browsers
+  web3 = new Web3(window.ethereum);
+  try { 
+    window.ethereum.enable().then(
+      function() {
+        console.log("Web3 Enabled");
+      }
+    );
+  } catch(e) {
+    console.log(e);
+  }
+} else if(window.web3) { //Legacy DApp Browsers
+  web3 = new Web3(web3.currentProvider);
+  console.log("Legacy web3 enabled");
+} else { //Non-DApp Browsers
+  alert('Please install MetaMask');
+}
 
 serviceWorker.unregister();
