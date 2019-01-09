faad59e3d9cc22ace747c21956b5f8ce2fddc3da
James Quinn <flash.jaq@gmail.com>
2019-01-09 18:01:12 +0100

    Removed redux dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
 
 import Main from "./components/main/Landing";
-
-import store from "./store";
 
 import "./App.css";
 
 class App extends Component {
   render(){
     return(
-      <Provider store={store}>
-        <div className="App">
-          <Main />
-        </div>
-      </Provider>
+      <div className="App">
+        <Main />
+      </div>
     );
   }
 }
