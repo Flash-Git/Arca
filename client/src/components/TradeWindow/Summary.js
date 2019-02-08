0a7052ed75f2426881b0c641875fcef349ecaa97
Flash <flash.jaq@gmail.com>
2019-02-08 15:39:16 +0000

    Improved ENS display

--- client/src/components/TradeWindow/Summary.js
+++ client/src/components/TradeWindow/Summary.js
@@ -6,19 +6,21 @@ class Summary extends Component {
   render(){
     return(
       <div className="method" style={ methodStyle }>
-        ENS - { this.props.address } - { this.props.address !== "" ? <img src={makeBlockie(this.props.address)} width="22px" alt="blockie" /> : "" }
+        ENS - { this.props.address } &nbsp; { this.props.address !== "" ? <img src={makeBlockie(this.props.address)} width="22px" height="22px" alt="blockie" style={{ marginTop:"0.2em" }} /> : "" }
       </div>
     );
   }
 }
 
 const methodStyle = {
+  display: "flex",
   gridColumn: "1 / 3",
   gridRow: "1",
   textAlign: "center",
   justifyContent: "center",
   background: "#222",
-  color: "#fff"
+  color: "#fff",
+  marginTop:"0.8rem"
 }
 
 //PropTypes
