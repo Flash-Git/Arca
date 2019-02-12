bf78533b4c8b8b14b15d97a8fe132b7352a908cf
Flash <flash.jaq@gmail.com>
2019-02-12 20:57:17 +0000

    Refactored web3 connection systems

--- client/src/App.js
+++ client/src/App.js
@@ -24,73 +24,59 @@ class App extends Component {
     userBox: userBoxStatus.NO_BOX
   }
 
-
-/*
-  button to connect to web3
-
-  isUser check for box purposes
-
-  checkConnected method for periodical use and fallback
-
-
-
-
-
-
-*/
-
-
+  checkConnection = () => {
+    try{
+      if(window.ethereum.networkVersion !== "4" && window.ethereum.selectedAddress !== "undefined"){
+        alert("The Ethereum contract is currently running on the rinkeby network.");
+        this.setState({ connected: false });
+        return 3;
+      }
+      if(!window.web3.utils.isAddress(window.ethereum.selectedAddress)){
+        this.setState({ connected: false });
+        return 2;
+      }
+    }catch(e){
+      this.setState({ connected: false });
+      console.log(e);
+      return 1;
+    }
+    this.setState({ connected: true });
+    return 0;
+  }
 
   enableWeb3 = () => {
+    const connection = this.checkConnection();
     //Check whether the DApp has an open connection to the Ethereum blockchain
-    try{
-      const add = window.ethereum.selectedAddress;
-    } catch(e){
-      
-    }
+    if(connection === 0) return;
+
+    //Check whether the user is on the rinkeby network
+    if(connection === 3) return;
     
-    
-      if(this.checkConnected()){
-      return;
-    }
-    
+    console.log("cunt");
+
     //Check if user's window has a window.ethereum currently available
-    if(typeof window.ethereum === "undefined"){
-      alert("Please install MetaMask");
-    }
+    if(typeof window.ethereum === "undefined") alert("Please install MetaMask");
 
     //Attempt to open a connection to the Ethereum blockchain
     window.web3 = new Web3(window.ethereum);//const web3 = new Web3(window.web3.currentProvider);
     window.ethereum.enable()
-    .then(accounts => this.checkConnected())
-    .catch(e => this.checkConnected());
-
-  }
-
-  checkConnected = () => {
-    this.isUser();
-    //console.log("Checking");
-    try{
-      if(window.web3.utils.isAddress(window.ethereum.selectedAddress)){
-        this.setState({ connected: true });
-        return true;
+    .then(accounts => this.checkConnection())
+    .catch(e => {
+      if(e !== "User rejected provider access"){
+        alert("There was an issue signing you in. Please check console for error");
+        console.log("Error:\n" + e);
+        return;
       }
-      this.setState({ connected: false });
-      return false;
-    } catch(e){
-      this.setState({ connected: false });
-      console.log(e);
-      return false;
-    }
+    });
   }
 
   setAddresses = (addresses) => {
     this.setState({ addresses }, () => {
-      this.isUser();
+      this.checkUserBox();
     });
   }
 
-  isUser = () => {
+  checkUserBox = () => {
     try{
       if(this.state.addresses[0].toUpperCase() === window.ethereum.selectedAddress.toUpperCase()){
         this.setState({ userBox: userBoxStatus.FIRST_BOX });
@@ -102,88 +88,9 @@ class App extends Component {
     } catch(e){
       console.log("Provider address not found");
       console.log("Enabling Ethereum...");
-      window.web3 = new Web3(window.ethereum);
-      window.ethereum.enable()
-        .then(accounts => console.log("Address found: " + accounts[0]))
-        .catch(e => console.log(e + "Failed to connect"));
+      this.enableWeb3();
     }
   }
-
-
-  
-
-
-
-  //   .then(accounts => {
-  //     // You now have an array of accounts!
-  //     // Currently only ever one:
-  //     // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
-  //     console.log(accounts[0]);
-  //   })
-  //   .catch(reason => {
-  //     console.error(reason);
-  //   });
-  //   this.checkConnected();
-  //   // web3.eth.getTransactionCount("0x2558aDC54E307Bef0c2B8D5bAcc4Bc9c53154EAc")
-  //   //   .then(num => console.log(num));
-  // }
-
-
-    // if(typeof window.ethereum === 'undefined'){
-    //   alert('Looks like you need a Dapp browser to get started.')
-    //   alert('Consider installing MetaMask!')
-    // } else {
-
-    //   // In the case the user has MetaMask installed, you can easily
-    //   // ask them to sign in and reveal their accounts:
-    //   window.ethereum.enable()
-    
-    //   // Remember to handle the case they reject the request:
-    //   .catch(function (reason) {
-    //     if(reason === 'User rejected provider access') {
-    //       // The user didn't want to sign in!
-    //     } else {
-    //       // This shouldn't happen, so you might want to log this...
-    //       alert('There was an issue signing you in.')
-    //     }
-    //   })
-    
-    //   // In the case they approve the log-in request, you'll receive their accounts:
-    //   .then(function (accounts) {
-    //     //console.log("ethereum: " + ethereum);
-    //     // You also should verify the user is on the correct network:
-
-    //     // if(ethereum.networkVersion !== 4) {
-    //     //   alert('This application requires the rinkeby network, please switch it in your MetaMask UI.')
-    //     // }
-    //     const account = accounts[0];
-    //     console.log("Account " + account);
-    //   });
-    // }
-
-
-
-    // let web3;
-    // if(window.ethereum) { //Modern DApp Browsers
-    //   web3 = new Web3(window.ethereum);
-    //   try {
-    //     window.ethereum.enable().then(
-    //       function() {
-    //         console.log("Web3 Enabled");
-    //       }
-    //     )
-    //   } catch(e) {
-    //     console.log(e);
-    //   }
-    // } else if(window.web3) { //Legacy DApp Browsers
-    //   web3 = new Web3(web3.currentProvider);
-    //   console.log("Legacy web3 enabled");
-    // } else { //Non-DApp Browsers
-    //   alert('Please install MetaMask');
-    // }
-    // this.setState({ web3 });
-  //   }
-  // }
 
   refresh = () => {
 
