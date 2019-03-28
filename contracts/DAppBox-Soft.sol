2a29bfe7bd9a69143b7e38144d32214530d3f3a4
James Quinn <flash.jaq@gmail.com>
2019-03-28 12:18:24 +0100

    Added an interface for Erc721

--- contracts/DAppBox-Soft.sol
+++ contracts/DAppBox-Soft.sol
@@ -7,15 +7,35 @@ pragma solidity ^0.5.7;
 */
 
 interface Erc20 {
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
   function transfer(address to, uint256 value) external returns (bool);
   function approve(address spender, uint256 value) external returns (bool);
   function transferFrom(address from, address to, uint256 value) external returns (bool);
   function totalSupply() external view returns (uint256);
   function balanceOf(address who) external view returns (uint256);
   function allowance(address owner, address spender) external view returns (uint256);
-  
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract Erc721 {
+  event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+  event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+  event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+  function balanceOf(address owner) public view returns (uint256 balance);
+  function ownerOf(uint256 tokenId) public view returns (address owner);
+
+  function approve(address to, uint256 tokenId) public;
+  function getApproved(uint256 tokenId) public view returns (address operator);
+
+  function setApprovalForAll(address operator, bool _approved) public;
+  function isApprovedForAll(address owner, address operator) public view returns (bool);
+
+  function transferFrom(address from, address to, uint256 tokenId) public;
+  function safeTransferFrom(address from, address to, uint256 tokenId) public;
+
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public;
 }
 
 contract DAppBoxSoft {
