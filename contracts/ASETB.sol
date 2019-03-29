737c406ef263c2333086a22dddb960e8e9fa6c11
James Quinn <flash.jaq@gmail.com>
2019-03-29 11:16:37 +0100

    Created basic ERC20 token

---
+++ contracts/ASETB.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import "./ERC20.sol";
+
+/*
+* @title Another Standard ERC20 Token -Balanceable
+*
+* @dev Implementation of the basic standard token
+* https://eips.ethereum.org/EIPS/eip-20
+*/
+contract ASETB is ERC20 {
+  /*
+  * Constructor
+  * @dev Gives all tokens to contract creator
+  */
+  constructor() public {
+    name = "Another Standard ERC Token -Balanceable";
+    symbol = "ASETB";
+    decimals = 18;
+    _totalSupply = 1000000000*10**uint256(decimals);
+    _balances[msg.sender] = _totalSupply;
+  }
+}
