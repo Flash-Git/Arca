583c6272f5be54ac179eb765576918cb604ae0da
James Quinn <flash.jaq@gmail.com>
2019-04-02 18:30:39 +0100

    Updated ERC721 abi to take name and symbol

--- client/src/abiErc721.js
+++ client/src/abiErc721.js
@@ -1,5 +1,33 @@
 const abiErc721 = [
 	{
+    "constant": true,
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+  {
 		"constant": true,
 		"inputs": [
 			{
