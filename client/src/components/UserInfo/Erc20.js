ecfbf3a1d9d57d538f8f7ed1abbfb8bc3816e961
James Quinn <flash.jaq@gmail.com>
2019-04-06 23:58:24 +0100

    Added button for adding erc to offer

--- client/src/components/UserInfo/Erc20.js
+++ client/src/components/UserInfo/Erc20.js
@@ -3,21 +3,45 @@ import PropTypes from "prop-types";
 import makeBlockie from "ethereum-blockies-base64";
 
 class Erc20 extends Component {
+
+  add = (e) => {
+    this.props.addErc(this.props.erc);
+  }
+
   render() {
     return(
       <div className="erc20" style={ erc20Style }>
         <li style={ liStyle }>
-          <span style={ symbolStyle }>{ this.props.symbol }&nbsp;</span>
-          <img src={makeBlockie(this.props.contractAdd)} width="8px" height="8px" alt="blockie" style={{ marginTop:"0.2em" }} /><br/>
-          Balance:&nbsp;{ this.props.balance }<br/>
-          Allowance:&nbsp;{ this.props.allowance }
+          <div>
+            <span style={ symbolStyle }>{ this.props.erc.symbol }&nbsp;</span>
+            <img src={ makeBlockie(this.props.erc.contractAdd) } width="11px" height="10px" alt="blockie" style={{ marginTop:"0.2em" }} />&nbsp;<br/>
+            Balance:&nbsp;{ this.props.erc.balance }<br/>
+            Allowance:&nbsp;{ this.props.erc.allowance }
+          </div>
+          <button onClick={ this.add } style={ btnStyle }>
+            +
+          </button>
         </li>
       </div>
     );
   }
 }
 
+const btnStyle = {
+  marginLeft: "auto",
+  textAlign: "center",
+  justifyContent: "center",
+  background: "#bbb",
+  border: "none",
+  borderRadius: "30%",
+  fontWeight: "bold",
+  padding: "0.3rem",
+  alignSelf: "center",
+  color: "000"
+}
+
 const liStyle= {
+  display: "flex",
   listStyleType: "none",
   marginTop: "0.4rem",
   padding: "0",
@@ -31,6 +55,7 @@ const erc20Style = {
 }
 
 const symbolStyle = { 
+  fontSize: "115%",
   fontWeight: "bold",
   marginLeft: "2rem"
 }
@@ -38,9 +63,8 @@ const symbolStyle = {
 //PropTypes
 Erc20.propTypes = {
   contractAdd: PropTypes.string.isRequired,
-  symbol: PropTypes.string.isRequired,
-  balance: PropTypes.string.isRequired,
-  allowance: PropTypes.string.isRequired
+  erc20: PropTypes.object.isRequired,
+  addErc20: PropTypes.func.isRequired  
 }
 
 export default Erc20;
