784556efc49f66a3f810d7422f1dfe47082925aa
James Quinn <flash.jaq@gmail.com>
2019-04-07 17:14:20 +0100

    Added allowance tx

--- client/src/components/TradeWindow/SubmitBox.js
+++ client/src/components/TradeWindow/SubmitBox.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import uuid from "uuid/v4";
 import PropTypes from "prop-types";
 
-import { sendStatus } from "../../Static";
+import abiErc20 from "../../abis/abiErc20";
+import abiErc721 from "../../abis/abiErc721";
+import { AppAddress, sendStatus } from "../../Static";
 
 class SubmitBox extends Component {
 
@@ -19,6 +21,7 @@ class SubmitBox extends Component {
     }
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.allowErc = this.allowErc.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -27,13 +30,15 @@ class SubmitBox extends Component {
     }
   }
 
-  onSubmit(e) {
+  onSubmit(e) {//addOffer
     e.preventDefault();
     const method = this.state;
     if(method.type.includes("20")){
       method.type = 0;
-    } else if(method.type.includes("721")){
+    }else if(method.type.includes("721")){
       method.type = 1;
+    }else{
+      return;
     }
     method.id = method.type+"-"+uuid();//TODO getCount
     this.setState(method);
@@ -46,35 +51,94 @@ class SubmitBox extends Component {
     });
   }
 
+  async allowErc(e) {
+    e.preventDefault();
+    if(!this.props.connected){
+      return;
+    }
+
+    let ercAbi;
+    let type;
+
+    if(this.state.type.includes("20")){
+      ercAbi = abiErc20;
+      type = 0;
+    }else if(this.state.type.includes("721")){
+      ercAbi = abiErc721;
+      type = 1;
+    }else{
+      return;
+    }
+
+    try{
+      let contract = await new window.web3.eth.Contract(ercAbi, this.state.contractAdd);
+
+      if(type === 0){//ERC20
+        await contract.methods.approve(AppAddress, "10000000000000000000000000000000000").send({
+          from: this.props.address
+        })
+        .on("transactionHash", (hash) => {
+          console.log("txHash: " + hash);
+        })
+        .on("confirmation", (confirmationNumber, receipt) => {
+          if(confirmationNumber === 3){
+            console.log("receipt: " + receipt);
+          }
+        })
+        .on("error", console.error);
+      }else{//ERC721
+        await contract.methods.setApprovalForAll(AppAddress, true).send({
+          from: this.props.address
+        })
+        .on("transactionHash", (hash) => {
+          console.log("txHash: " + hash);
+        })
+        .on("confirmation", (confirmationNumber, receipt) => {
+          if(confirmationNumber === 3){
+            console.log("receipt: " + receipt);
+          }
+        })
+        .on("error", console.error);
+      }
+    }catch(e){
+      console.log(e);
+      return;
+    }
+  }
+
   render() {
     return(
       <form onSubmit={ this.onSubmit } className="method" style={ methodStyle }>
-        <input 
-          type="text" 
-          name="contractAdd" 
-          placeholder="Token's Contract Address" 
+        <input
+          type="text"
+          name="contractAdd"
+          placeholder="Token's Contract Address"
           value={ this.state.contractAdd }
           onChange={ this.onChange }
         />
         <input
-          type="text" 
-          name="type" 
+          type="text"
+          name="type"
           placeholder="ERC20"
           value={ this.state.type }
           onChange={ this.onChange }
         />
         <input
-          type="text" 
-          name="amountId" 
-          placeholder="Amount / ID" 
+          type="text"
+          name="amountId"
+          placeholder="Amount / ID"
           value={ this.state.amountId }
           onChange={ this.onChange }
         />
         <input
-          type="submit" 
-          value="Add Offer" 
+          type="submit"
+          name="addOffer"
+          value="Add Offer"
           className="btn"
         />
+        <button onClick={ this.allowErc } className="btn">
+          Enable
+        </button>
       </form>
     );
   }
@@ -92,7 +156,9 @@ const methodStyle = {
 //PropTypes
 SubmitBox.propTypes = {
   addMethod: PropTypes.func.isRequired,
-  erc: PropTypes.object.isRequired
+  erc: PropTypes.object.isRequired,
+  connected: PropTypes.bool.isRequired,
+  address: PropTypes.string.isRequired
 }
 
 export default SubmitBox;
