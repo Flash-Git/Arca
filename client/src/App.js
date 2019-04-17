2a9f27eeb44f926781083a471b639338c84dcbec
James Quinn <flash.jaq@gmail.com>
2019-04-17 19:34:07 +0200

    Moved header back to Top

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import UserInfo from "./components/UserInfo";
 import TradeWindow from "./components/TradeWindow";
 import PreTrade from "./components/PreTrade";
-//import Help from "./components/Help";
 
 import { userBoxStatus } from "./Static";
 import "./App.css";
@@ -23,7 +22,7 @@ class App extends Component {
   checkConnection = () => {
     try{
       //Disable if using Embark
-      if(window.ethereum.networkVersion !== "4" && window.ethereum.selectedAddress){
+      if(window.ethereum.networkVersion !== "4"){
         alert("The Ethereum contract is currently running on the rinkeby network.");
         this.setState({ connected: false });
         return 3;
@@ -37,12 +36,6 @@ class App extends Component {
       return 1;
     }
     this.setState({ connected: true });
-    //BROKEN
-    // window.web3.eth.ens.resolver('ethereum.eth').then((contract) => {
-    //   console.log(contract);
-    // });
-    //window.web3.eth.ens.getAddress("ethereum.eth").then(add1 => console.log(add1))
-    
     return 0;
   }
 
@@ -51,7 +44,7 @@ class App extends Component {
     //Check whether the DApp has an open connection to the Ethereum blockchain
     if(connection === 0) return;
     
-    //Check whether the user is on the rinkeby network
+    //Check whether the user is on the correct network
     if(connection === 3) return;
     
     //Check if user's window has a window.ethereum currently available
@@ -107,14 +100,14 @@ class App extends Component {
   render() {
     return(
       <div className="App">
+        <Header />
         <UserInfo enableWeb3={ this.enableWeb3 } connected ={ this.state.connected } addErc={ this.addErc } />
-        <div style={ mainStyle } >
-          <Header />
-          <PreTrade refresh={ this.refresh } setAddresses={ this.setAddresses }
-            isUser={ this.state.userBox } connected={ this.state.connected }/>
-          {/*<Help />*/}
-          <TradeWindow addresses={ this.state.addresses } ensAdds={ this.state.ensAdds }
-            userBox={ this.state.userBox } connected={ this.state.connected } erc={ this.state.erc } />
+          <div style={ mainStyle } >
+            <PreTrade refresh={ this.refresh } setAddresses={ this.setAddresses }
+              isUser={ this.state.userBox } connected={ this.state.connected }/>
+            {/*<Help />*/}
+            <TradeWindow addresses={ this.state.addresses } ensAdds={ this.state.ensAdds }
+              userBox={ this.state.userBox } connected={ this.state.connected } erc={ this.state.erc } />
           </div>
       </div>
     );
