2299161742eb479ea219574a47752e2e7bce45ba
James Quinn <flash.jaq@gmail.com>
2019-04-30 21:33:10 +0200

    Removed duplicate width

--- client/src/components/TradeWindow/Box.js
+++ client/src/components/TradeWindow/Box.js
@@ -225,7 +225,6 @@ const boxStyle = {
   color: colours.Quaternary,
   fontWeight: "normal",
   minWidth: "10rem",
-  width: "50rem",
   padding: "0.3rem",
   boxShadow: "0px 0px 25px -2px rgba(0,0,0,0.25)",
   width: "100%"//
