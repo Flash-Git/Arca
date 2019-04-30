78892c03431daec9a709934db53a6229132689dc
James Quinn <flash.jaq@gmail.com>
2019-04-30 05:17:32 +0200

    Added bitcoin address

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,7 +14,7 @@ class Footer extends Component {
           <a style={ linkStyle } href="https://etherscan.io/address/jaquinn.eth">
             <FontAwesomeIcon style={ iconStyle } icon={ faEthereum } />&nbsp;Donations
           </a>
-          <a style={ linkStyle } href="/">
+          <a style={ linkStyle } href="https://blockstream.info/address/bc1qvn7u88k5qpq2m7g8mu8swh88v7q59msxa4w6vg">
             <FontAwesomeIcon style={ iconStyle } icon={ faBitcoin } />&nbsp;Donations
           </a>
           <a style={ linkStyle } href="https://github.com/Flash-Git/Arca">
