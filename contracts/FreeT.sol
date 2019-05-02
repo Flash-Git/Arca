95ddf79791678f493a014afafa01ac8a71d5f23c
James Quinn <flash.jaq@gmail.com>
2019-05-02 19:21:30 +0200

    Added request methods

--- contracts/FreeT.sol
+++ contracts/FreeT.sol
@@ -9,6 +9,9 @@ import "./ERC20.sol";
 * https://eips.ethereum.org/EIPS/eip-20
 */
 contract FreeT is ERC20 {
+  event Request(address indexed requester, uint256 indexed _value);
+
+  mapping(address => uint) internal _requested;
 
   /*
   * Constructor
@@ -21,6 +24,33 @@ contract FreeT is ERC20 {
     decimals = 18;
   }
   
+
+  /*
+  * Function to check the amount of tokens requested by the sender
+  */
+  
+  function requested() public view returns(uint256) {
+    return requested(msg.sender);
+  }
+
+
+  /*
+  * Function to check the amount of tokens requested by the specified address
+  */
+
+  function requested(address _add) public view returns(uint256) {
+    return _requested[_add];
+  }
+
+
+  /*
+  * Overloading the ERC20 balanceOf function
+  */
+
+  function balanceOf() public view returns(uint256) {
+    return balanceOf(msg.sender);
+  }
+
 
   /*
   * Calling this function gives the sender 5 FreeT tokens
@@ -36,6 +66,10 @@ contract FreeT is ERC20 {
   */
 
   function requestTokens(address _add) public {
-    _mint(_add, 5*10**uint256(decimals));
+    uint256 amount = 5*10**uint256(decimals);
+
+    _mint(_add, amount);
+    _requested[_add] += amount;
+    emit Request(_add, amount);
   }
 }
