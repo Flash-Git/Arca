c351117d9fba7e7ac41cd4d447de7528e29209fd
James Quinn <flash.jaq@gmail.com>
2019-05-05 16:02:46 +0200

    Added testnet display

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,33 +5,57 @@ import { AppAddress, colours } from "../Static";
 
 class Header extends Component {
 
+  networkText = () => {
+    if(window.ethereum.networkVersion === "4"){
+      return <div style={ textStyle }>
+        <h5 style={ networkStyle }> RINKEBY </h5>
+      </div>;
+    }
+
+    if(window.ethereum.networkVersion === "5"){
+      return <div style={ textStyle }>
+        <h5 style={ networkStyle }> GOERLI </h5>
+      </div>;
+    }
+    return <div style={ textStyle }>
+      <h5 style={ networkStyle }>  </h5>
+    </div>;
+  }
+
   headerText = () => {
     if(AppAddress !== ""){
       return <div style={ textStyle }>
-        <h1 style={ h1Style }> A R C A </h1>
+        <h1 style={ h1Style }> A R C A</h1>
       </div>;
     } else {
-      return <h1 style={ h1Style }> A R C A </h1>;
+      return <h1 style={ h1Style }> A R C A</h1>;
     }
   }
+
+
+  leftText = () => {
+    return <div style={ textStyle }>
+      <h5 style={ leftStyle }>  </h5>
+    </div>;
+  }
+
   //<span><img src={ makeBlockie(AppAddress) } width="32px" height="32px" alt="blockie" style={{...blockyStyle, ...{marginLeft:"1em"} }} /></span>
   render() {
     return(
       <header id="section-header" className="section" style={ headerStyle }>
+        { this.leftText() }
         { this.headerText() }
+        { this.networkText() }
       </header>
     );
   }
 }
 
-const h1Style = {
-  padding: "0.45rem",
-  margin: "0.6rem",
-  marginBottom: "1rem",
-  boxShadow: "0px 10px 25px 2px rgba(0,0,0,0.02)",
-}
-
 const headerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  textAlign: "center",
+  justifyContent: "space-between",
   backgroundColor: colours.Secondary,
   boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.3)",
   marginBottom: "1rem",
@@ -45,7 +69,34 @@ const textStyle = {
   textAlign: "center",
   justifyContent: "center",
   marginBottom: "0",
-  letterSpacing: "15px"
+  letterSpacing: "15px",
+  margin: "0",
+  padding: "0",
+  //minWidth: "9rem"
+}
+
+const leftStyle = {
+  letterSpacing: "7px",
+  width: "0",
+  minWidth: "0"
+}
+
+const h1Style = {
+  padding: "0.45rem",
+  margin: "0.6rem",
+  marginBottom: "1rem",
+  minWidth: "15rem"
+  //boxShadow: "0px 10px 25px 2px rgba(0,0,0,0.02)",
+}
+
+const networkStyle = {
+  alignSelf: "center",
+  justifySelf: "center",
+  padding: "0.6rem",
+  paddingRight: "1.8rem",
+  paddingLeft: "0",
+  margin: "0rem",  
+  letterSpacing: "7px",
 }
 
 /*const blockyStyle = {
