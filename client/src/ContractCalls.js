e7028af6cb35ce6f82658a8117001e2aa57b8f00
James Quinn <flash.jaq@gmail.com>
2019-05-13 22:09:07 +0200

    Added calls

--- client/src/ContractCalls.js
+++ client/src/ContractCalls.js
@@ -11,19 +11,46 @@ function newContract(_abi, _add) {
   }
 }
 
-export function arcaContract() {
+export function ArcaContract() {
   return newContract(abiArca, AppAddress());
 }
 
-export function erc20Contract(_add) {
+export function ArcaCalls(_method, _params, _contract = ArcaContract()) {
+  switch(_method){
+    case "getErc20Count":
+      return _contract.methods.getErc20Count(_params[0], _params[1]).call({
+        from: window.ethereum.selectedAddress
+      });
+    case "getErc721Count":
+      return _contract.methods.getErc721Count(_params[0], _params[1]).call({
+        from: window.ethereum.selectedAddress
+      });
+    case "getNonce":
+      return _contract.methods.getNonce(_params[0], _params[1]).call({
+        from: window.ethereum.selectedAddress
+      });
+    case "getOfferErc20":
+      return _contract.methods.getOfferErc20(_params[0], _params[1], _params[2]).call({
+        from: window.ethereum.selectedAddress
+      });
+    case "getOfferErc721":
+      return _contract.methods.getOfferErc721(_params[0], _params[1], _params[2]).call({
+        from: window.ethereum.selectedAddress
+      });
+    default:
+      return false;
+  }
+}
+
+export function Erc20Contract(_add) {
   return newContract(abiErc20, _add);
 }
 
-export function erc721Contract(_add) {
+export function Erc721Contract(_add) {
   return newContract(abiErc721, _add);
 }
 
-export function ercCalls(_contract, _method) {
+export function ErcCalls(_method, _contract) {
   switch(_method){
     case "decimals":
       return _contract.methods.decimals().call({
