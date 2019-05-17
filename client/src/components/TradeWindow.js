dfe105fa7aabb55c0c1676b138e9b4a367f043bd
James Quinn <flash.jaq@gmail.com>
2019-05-17 12:55:56 +0200

    Added Trade Executed message

--- client/src/components/TradeWindow.js
+++ client/src/components/TradeWindow.js
@@ -6,25 +6,31 @@ import { boolStatus, userBoxStatus } from "../Static";
 
 class TradeWindow extends Component {
   
-  state = {
-    executedStatus: boolStatus.FALSE
-  }
-  
-  execute = () => {
-    if(!this.props.connected){
-      alert("Not connected");
-      return;
+  constructor(props) {
+    super(props);
+    this.state = {
+      satisfied: [boolStatus.FALSE, boolStatus.FALSE],
+      executedStatus: boolStatus.FALSE
     }
-    if(this.props.userBox === userBoxStatus.FIRST_BOX){
-      this.sendExecute(this.props.addresses[0], this.props.addresses[1]);
-    } else if(this.props.userBox === userBoxStatus.SECOND_BOX){
-      this.sendExecute(this.props.addresses[1], this.props.addresses[0]);
-    } else {
-      return;
-    }
-    this.setState({ executedStatus: boolStatus.TOTRUE });
+    this.setSatisfied = this.setSatisfied.bind(this);
   }
 
+  setSatisfied(_boxNum, _satisfied) {
+    console.log(_satisfied);
+    let satisfied = this.state.satisfied;
+    if(satisfied[_boxNum] ===_satisfied) return;
+
+    satisfied[_boxNum] = _satisfied;
+    this.setState({ satisfied });
+    
+    if(satisfied[0] === boolStatus.TRUE && satisfied[1] === boolStatus.TRUE){
+      this.setState({ executedStatus: boolStatus.TRUE });
+      alert("Trade Successfully Executed");
+    }else{
+      this.setState({ executedStatus: boolStatus.FALSE });
+    }
+  }
+  
   render() {
     return(
       <div id="section-tradeWindow" className="section" style={ tradeWindowStyle }>
@@ -33,12 +39,12 @@ class TradeWindow extends Component {
           <Box connected={ this.props.connected } counter={ this.props.counter } 
             isUser={ (this.props.userBox === userBoxStatus.SECOND_BOX ? true : false) }
             addresses={ [this.props.addresses[1], this.props.addresses[0]] } ensAdd={ this.props.ensAdds[1] }
-            boxNum = { 0 } erc={ this.props.erc } 
+            boxNum = { 0 } erc={ this.props.erc } setSatisfied={ this.setSatisfied }
           />
           <Box connected={ this.props.connected } counter={ this.props.counter }
             isUser={ (this.props.userBox === userBoxStatus.FIRST_BOX ? true : false) }
             addresses={ [this.props.addresses[0], this.props.addresses[1]] } ensAdd={ this.props.ensAdds[0] }
-            boxNum = { 1 } erc={ this.props.erc }
+            boxNum = { 1 } erc={ this.props.erc } setSatisfied={ this.setSatisfied }
           />
         </div>
       </div>
@@ -64,12 +70,12 @@ const boxesStyle = {
 }
 //PropTypes
 TradeWindow.propTypes = {
-  userBox: PropTypes.number.isRequired,
+  connected: PropTypes.bool.isRequired,
+  counter: PropTypes.number.isRequired,
   addresses: PropTypes.array.isRequired,
   ensAdds: PropTypes.array.isRequired,
-  connected: PropTypes.bool.isRequired,
-  erc: PropTypes.object.isRequired,
-  counter: PropTypes.number.isRequired
+  userBox: PropTypes.number.isRequired,
+  erc: PropTypes.object.isRequired
 }
 
 export default TradeWindow;
