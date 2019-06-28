318cce338967801dd0b7cdbf6a565cf34676bc46
James Quinn <flash.jaq@gmail.com>
2019-06-28 22:07:58 +0200

    Added Alias

---
+++ client/src/components/Alias.js
@@ -0,0 +1,35 @@
+import ENS from "ethereum-ens";
+
+
+export function KnownContracts(_address) {
+  try{
+    if(window.ethereum.networkVersion === "1"){ //Live
+      //ens
+      switch(_address){
+        case "0xFaC7BEA255a6990f749363002136aF6556b31e04":
+          return "ENS;
+        default:
+          return _address;
+      }
+    }
+
+    if(window.ethereum.networkVersion === "4"){ //Rinkeby
+      //ens
+      switch(_address){
+        default:
+          return _address;
+      }
+    }
+    if(window.ethereum.networkVersion === "5"){ //Goerli
+      //ens
+      switch(_address){
+        default:
+          return _address;
+      }
+    }
+  }catch(e){
+    console.log("Error in KnownContracts:");
+    console.log(e);
+    return e;
+
+}
