850d06e953c465c4605b4ba5bd7db360717437c9
James Quinn <flash.jaq@gmail.com>
2019-06-29 22:35:16 +0200

    Added initial Utils contract

---
+++ contracts/Utils.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.10;
+
+/*
+* @Author Flash
+* Source at https://github.com/Flash-Git/Arca/tree/master/contracts
+*/
+
+/*
+* @Author Flash
+* @title ENSUtils
+*
+* @dev Utility contract
+*/
+
+contract Utils {
+  //@dev get the ENS erc721 ID
+  function namehashToID(bytes32 _namehash) view returns(uint32) {
+    return uint32(_namehash);
+  }
+}
