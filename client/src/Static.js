4b5b5e92beb5d84d48288b8550dafe6c0205cc4d
Flash <flash.jaq@gmail.com>
2019-06-29 20:20:28 +0200

    Added preliminary handling of mainnet

--- client/src/Static.js
+++ client/src/Static.js
@@ -1,5 +1,8 @@
 function ListErc20() {
   try{
+    if(window.ethereum.networkVersion === "1"){
+      return ["0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"];
+    }
     if(window.ethereum.networkVersion === "4"){ //Rinkeby
       return ["0xb88bf768914648cBaa771816a95122a4Da720f80", "0xF256D0FFD6B42653269881356CF68D178dfc907d", "0xc778417E063141139Fce010982780140Aa0cD5Ab", "0x0A057a87CE9C56D7e336B417c79cf30E8d27860B",
         "0x275A5B346599b56917e7B1C9de019DCf9EaD861a", "0x3615757011112560521536258c1E7325Ae3b48AE",
@@ -17,6 +20,9 @@ function ListErc20() {
 
 function ListErc721() {
   try{
+    if(window.ethereum.networkVersion === "1"){
+      return ["0xFaC7BEA255a6990f749363002136aF6556b31e04"];
+    }
     if(window.ethereum.networkVersion === "4"){ //Rinkeby
       return ["0x552DcaEBAF0F311fB43977Fa31f85F776217f446"];
     }
@@ -31,6 +37,9 @@ function ListErc721() {
 
 function AppAddress() {
   try{
+    if(window.ethereum.networkVersion === "1"){
+      return "0xb8F590D50D7d58A5BCd1e669209375026aFb1123";
+    }
     if(window.ethereum.networkVersion === "4"){ //Rinkeby
       return "0x255bca69542f6515af3b172223e903dfb302038b";
     }
