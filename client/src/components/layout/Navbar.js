3ba7dbd70dda7359363c1f7ae5b0a5e5357c9745
Flash <flash.jaq@gmail.com>
2019-07-20 16:31:04 +0200

    Refactored navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,93 +1,55 @@
-import React from "react";
-//import makeBlockie from "ethereum-blockies-base64";
+import React, { Fragment, useContext } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import PropTypes from "prop-types";
 
-import { colours } from "../../Static";
-
-const Navbar = () => {
+const Navbar = ({ title, icon }) => {
   const connected = true;
   const network = 1;
 
-  const networkText = network => {
+  const networkText = () => {
     switch (network) {
       case 1:
-        return "LIVE";
+        return "Network: LIVE";
       case 4:
-        return "RINKEBY";
+        return "Network: RINKEBY";
       case 5:
-        return "GOERLI";
+        return "Network: GOERLI";
       default:
-        return "NETWORK";
+        return "Network: UNSUPPORTED";
     }
   };
 
   return (
-    <header id="section-header" className="section" style={headerStyle}>
-      <div style={textStyle}>
-        <h5 style={leftStyle}> </h5>
-      </div>
-      <h1 style={h1Style}> A R C A</h1>
-      <div style={textStyle}>
-        <h5 style={networkStyle}>
-          {!connected ? "Network" : networkText(network)}
-        </h5>
-      </div>
-    </header>
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <FontAwesomeIcon icon={icon} /> {title}
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">
+            {!connected ? "Not Connected" : networkText(network)}
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
-const headerStyle = {
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-  textAlign: "center",
-  width: "100%",
-  backgroundColor: colours.Primary,
-  boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.3)",
-  marginBottom: "1rem"
+Navbar.defaultProps = {
+  title: "A R C A",
+  icon: ["fas", "box-open"]
 };
 
-const textStyle = {
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  textAlign: "center",
-  color: colours.Secondary,
-  letterSpacing: "15px",
-  padding: "0",
-  margin: "0"
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.array.isRequired
 };
-
-const leftStyle = {
-  width: "0",
-  minWidth: "0",
-  letterSpacing: "7px"
-};
-
-const h1Style = {
-  minWidth: "15rem",
-  padding: "0.45rem",
-  margin: "0.6rem",
-  marginBottom: "1rem"
-};
-
-const networkStyle = {
-  alignSelf: "center",
-  justifySelf: "center",
-  letterSpacing: "7px",
-  padding: "0.6rem",
-  paddingRight: "1.8rem",
-  paddingLeft: "0",
-  margin: "0rem"
-};
-
-/*const blockyStyle = {
-  marginTop:"0.72em",
-  border:"solid 1px",
-  boxShadow: "0px 10px 15px 0px rgba(0,0,0,0.5)"
-}*/
-
-//PropTypes
-Navbar.propTypes = {};
 
 export default Navbar;
