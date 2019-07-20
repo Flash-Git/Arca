268c20ebd52f2d10f642f1a4fc9974deb41d4289
Flash <flash.jaq@gmail.com>
2019-07-20 13:27:21 +0200

    Added about page

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <p className="my-1">Ethereum Application</p>
+    <p className="bg-dark p">
+      <strong>Version: </strong> 1.0.0
+    </p>
+  </Fragment>
+);
+
+export default About;
