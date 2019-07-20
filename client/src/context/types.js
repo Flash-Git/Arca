60bdb94b25f006681ae64118a4db76999f5c5597
Flash <flash.jaq@gmail.com>
2019-07-20 02:59:10 +0200

    Added trade items

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,3 +1,7 @@
 // Alerts
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
+
+//Trade Items
+export const ADD_TRADE_ITEM = "ADD_TRADE_ITEM";
+export const REMOVE_TRADE_ITEM = "REMOVE_TRADE_ITEM";
