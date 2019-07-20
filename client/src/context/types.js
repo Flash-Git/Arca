552db949d409d931250c1bc2c01b0580b268fd10
Flash <flash.jaq@gmail.com>
2019-07-20 04:13:37 +0200

    Added user context

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -2,8 +2,14 @@
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
-//Trade Items
+// Trade Items
 export const ADD_TRADE_ITEM = "ADD_TRADE_ITEM";
 export const REMOVE_TRADE_ITEM = "REMOVE_TRADE_ITEM";
 export const SET_CURRENT_ITEM = "SET_CURRENT_ITEM";
 export const CLEAR_CURRENT_ITEM = "CLEAR_CURRENT_ITEM";
+
+// User
+export const SET_USER = "SET_USER";
+export const SET_TRADE_PARTNER = "SET_TRADE_PARTNER";
+export const CLEAR_USER = "CLEAR_USER";
+export const CLEAR_TRADE_PARTNER = "CLEAR_TRADE_PARTNER";
---
+++ client/src/context/user/UserState.js
@@ -0,0 +1,90 @@
+import React, { useReducer } from "react";
+
+import UserContext from "./UserContext";
+import UserReducer from "./UserReducer";
+
+import {
+  SET_USER,
+  SET_TRADE_PARTNER,
+  CLEAR_USER,
+  CLEAR_TRADE_PARTNER
+} from "../types";
+
+const UserState = () => {
+  const initialState = {
+    user: {
+      address: "",
+      ens: null,
+      balance: null,
+      ownedTokens: {
+        erc20Tokens: [],
+        erc721Tokens: []
+      },
+      requestedTokens: {
+        erc20Tokens: [],
+        erc721Tokens: []
+      }
+    },
+    tradePartner: {
+      address: "",
+      ens: null,
+      balance: null,
+      ownedTokens: {
+        erc20Tokens: [],
+        erc721Tokens: []
+      },
+      requestedTokens: {
+        erc20Tokens: [],
+        erc721Tokens: []
+      }
+    }
+  };
+
+  const [state, dispatch] = useReducer(UserReducer, initialState);
+
+  /*
+   * Actions
+   */
+
+  const setUser = user => {
+    dispatch({
+      type: SET_USER,
+      payload: user
+    });
+  };
+
+  const setTradePartner = tradePartner => {
+    dispatch({
+      type: SET_TRADE_PARTNER,
+      payload: tradePartner
+    });
+  };
+
+  const clearUser = () => {
+    dispatch({
+      type: CLEAR_USER
+    });
+  };
+
+  const clearTradePartner = () => {
+    dispatch({
+      type: CLEAR_TRADE_PARTNER
+    });
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user: state.user,
+        tradePartner: state.tradePartner,
+        setUser,
+        setTradePartner,
+        clearUser,
+        clearTradePartner
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+export default UserState;
