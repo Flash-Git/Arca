53110fae7885a863f915726e742ec50d828f886e
Flash <flash.jaq@gmail.com>
2019-07-20 15:08:28 +0200

    Added home page

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+import TradeBox from "../tradeBox/Box";
+
+const Home = () => {
+  useEffect(() => {
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <TradeBox />
+    </div>
+  );
+};
+
+export default Home;
