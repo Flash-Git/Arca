42d904a3fc154d2a666ed126b54eeb5b9b1e6d43
Flash <flash.jaq@gmail.com>
2019-07-20 13:26:17 +0200

    Added 404 page

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <div>
+    <h1>Not Found</h1>
+    <p className="lead">The page you are looking for does not exist.</p>
+  </div>
+);
+
+export default NotFound;
