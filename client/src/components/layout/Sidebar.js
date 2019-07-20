06e6f95bb35a647d4de0501a23a35737a59087ab
Flash <flash.jaq@gmail.com>
2019-07-20 18:22:40 +0200

    Formatting

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -6,18 +6,26 @@ const Sidebar = () => {
   const userContext = useContext(UserContext);
 
   const { settings, user, web3 } = userContext;
-  const { connected } = web3;
+  const { connected, loading } = web3;
 
   const tokens = () => (
     <ul>
       <h3>Erc20s</h3>
-      {user.ownedTokens.erc20Tokens.map(token => (
-        <li>{token.name}</li>
-      ))}
+      {user.ownedTokens.erc20Tokens.length > 0 ? (
+        user.ownedTokens.erc20Tokens.map(token => <li>{token.name}</li>)
+      ) : loading ? (
+        <Spinner />
+      ) : (
+        "No erc20s found"
+      )}
       <h3>Erc721s</h3>
-      {user.ownedTokens.erc721Tokens.map(token => (
-        <li>{token.name}</li>
-      ))}
+      {user.ownedTokens.erc721Tokens.length > 0 ? (
+        user.ownedTokens.erc721Tokens.map(token => <li>{token.name}</li>)
+      ) : loading ? (
+        <Spinner />
+      ) : (
+        "No erc721s found"
+      )}
     </ul>
   );
 
