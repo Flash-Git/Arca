6a126256c6df5b31562217a247f2d1de59edbe31
Flash <flash.jaq@gmail.com>
2019-07-22 18:53:40 +0200

    Added user box

--- client/src/components/tradeBox/Items.js
+++ client/src/components/tradeBox/Items.js
@@ -1,15 +1,21 @@
 import React, { useContext, useEffect } from "react";
 
+import PropTypes from "prop-types";
+
 import Erc20 from "./itemTypes/Erc20";
 import Erc721 from "./itemTypes/Erc721";
 import EnsForm from "./itemTypes/EnsForm";
 import TradeContext from "../../context/trade/TradeContext";
 
-const Items = () => {
+const Items = ({ user }) => {
   const tradeContext = useContext(TradeContext);
 
+  const items = user
+    ? tradeContext.user.tradeItems
+    : tradeContext.tradePartner.tradeItems;
+
   const internal = item => {
-    switch (type) {
+    switch (item.type) {
       case "erc20":
         return <Erc20 item={item} />;
       case "erc721":
@@ -22,12 +28,18 @@ const Items = () => {
   };
 
   return (
-    tradeContext.tradeItems.length > 0 &&
-    tradeContext.tradeItems.map(item => (
+    items.length > 0 &&
+    items.map(item => (
       <div>
-        <h3 className="text-primary">ID:{id}</h3>
-        {internal}
+        <h3 className="text-primary">ID:{item.id}</h3>
+        {internal(item)}
       </div>
     ))
   );
 };
+
+Items.propTypes = {
+  user: PropTypes.bool.isRequired
+};
+
+export default Items;
