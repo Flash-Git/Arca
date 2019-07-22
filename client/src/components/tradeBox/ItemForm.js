d46bb08930912a2f6bc6ab3de4bfe8cd581b5025
Flash <flash.jaq@gmail.com>
2019-07-22 18:53:31 +0200

    Added item form

---
+++ client/src/components/tradeBox/ItemForm.js
@@ -0,0 +1,62 @@
+import React, { useContext, useState } from "react";
+
+import TradeContext from "../../context/trade/TradeContext";
+
+const ItemForm = () => {
+  const tradeContext = useContext(TradeContext);
+
+  const { addTradeItem } = tradeContext;
+
+  const [item, setItem] = useState({
+    contractAdd: "",
+    type: "",
+    id: "" //id/amount
+  });
+
+  const { contractAdd, type, id } = item;
+
+  //Input
+  const onChange = e => {
+    setItem({ ...item, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    addTradeItem(item);
+  };
+
+  //Render
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Contract Address"
+        name="contractAdd"
+        value={contractAdd}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="type"
+        name="type"
+        value={type}
+        onChange={onChange}
+      />
+      <input
+        // TODO conditional
+        type="id"
+        placeholder="id"
+        name="id"
+        value={id}
+        onChange={onChange}
+      />
+      <input
+        type="submit"
+        value="Add Trade"
+        className="btn btn-primary btn-block"
+      />
+    </form>
+  );
+};
+
+export default ItemForm;
