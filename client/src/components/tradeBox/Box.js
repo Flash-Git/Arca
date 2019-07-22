979bc6cfdd62c69eebb69d0632cbc84b101b7c75
Flash <flash.jaq@gmail.com>
2019-07-22 18:53:21 +0200

    Added user box

--- client/src/components/tradeBox/Box.js
+++ client/src/components/tradeBox/Box.js
@@ -1,7 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Box = () => {
-  return <div>Box</div>;
+import ItemForm from "./ItemForm";
+import Items from "./Items";
+
+const Box = ({ user }) => {
+  return (
+    <div>
+      <Items user={user} />
+      {user && <ItemForm />}
+    </div>
+  );
+};
+
+Box.defaultProps = {
+  user: false
+};
+
+Box.propTypes = {
+  user: PropTypes.bool.isRequired
 };
 
 export default Box;
