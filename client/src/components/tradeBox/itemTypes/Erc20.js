92ccfae015d0da875bda1ae5a1d620921dbcfbc1
Flash <flash.jaq@gmail.com>
2019-07-22 14:25:27 +0200

    Added item types

---
+++ client/src/components/tradeBox/itemTypes/Erc20.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Erc20 = item => {
+  return <div>ERC20</div>;
+};
+
+Erc20.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+export default Erc20;
---
+++ client/src/components/tradeBox/itemTypes/Erc721.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Erc721 = item => {
+  return <div>ERC721</div>;
+};
+
+Erc721.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+export default Erc721;
