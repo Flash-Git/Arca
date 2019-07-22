3429fdf3f099a022c6952d0d85fe93d02c08645b
Flash <flash.jaq@gmail.com>
2019-07-22 18:55:24 +0200

    Added user box

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -3,6 +3,7 @@ export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
 // Trade Items
+export const SET_USER_BOX = "SET_USER_BOX";
 export const ADD_TRADE_ITEM = "ADD_TRADE_ITEM";
 export const REMOVE_TRADE_ITEM = "REMOVE_TRADE_ITEM";
 export const SET_CURRENT_ITEM = "SET_CURRENT_ITEM";
