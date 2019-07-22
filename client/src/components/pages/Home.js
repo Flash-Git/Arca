e5cf7dcb6946ffa24fcdde28836c5c6d84f89ef9
Flash <flash.jaq@gmail.com>
2019-07-22 18:53:14 +0200

    Added user box

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, Fragment } from "react";
 
-import TradeBox from "../tradeBox/Box";
+import Box from "../tradeBox/Box";
+import UserContext from "./../../context/user/UserContext";
+import TradeContext from "./../../context/trade/TradeContext";
 
 const Home = () => {
+  const userContext = useContext(UserContext);
+  const tradeContext = useContext(TradeContext);
+
+  const { userBox } = tradeContext;
+
   useEffect(() => {
     //eslint-disable-next-line
   }, []);
 
   return (
     <div>
-      <TradeBox />
+      {userBox === 0 ? <Box user={true} /> : <Box />}
+      {userBox === 1 ? <Box user={true} /> : <Box />}
     </div>
   );
 };
