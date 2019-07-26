5fba1366abdbd8e377fce5d7d50a3a18cef7c396
James Quinn <flash.jaq@gmail.com>
2019-07-26 18:10:08 +0200

    Added new item form

--- client/src/components/tradeBox/ItemForm.js
+++ client/src/components/tradeBox/ItemForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 
 import TradeContext from "../../context/trade/TradeContext";
 
@@ -8,12 +8,15 @@ const ItemForm = () => {
   const { addTradeItem } = tradeContext;
 
   const [item, setItem] = useState({
+    type: "ens",
     contractAdd: "",
-    type: "",
-    id: "" //id/amount
+    id: null,
+    amount: null,
+    name: null,
+    verified: true
   });
 
-  const { contractAdd, type, id } = item;
+  const { contractAdd, type, id, amount, name, verified } = item;
 
   //Input
   const onChange = e => {
@@ -22,37 +25,87 @@ const ItemForm = () => {
 
   const onSubmit = e => {
     e.preventDefault();
+
     addTradeItem(item);
+  };
+
+  const erc = () => (
+    <input
+      className="grow-2 address"
+      type="text"
+      placeholder="Contract Address"
+      name="contractAdd"
+      value={contractAdd}
+      onChange={onChange}
+    />
+  );
+
+  const form = () => {
+    switch (type) {
+      case "ens":
+        return (
+          <Fragment>
+            <input
+              className="grow-1"
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onChange}
+            />
+            {verified && (
+              <strong className=" grow-1 mx text-center">VERIFIED</strong>
+            )}
+          </Fragment>
+        );
+      case "erc20":
+        return (
+          <Fragment>
+            {erc()}
+            <input
+              className="grow-1"
+              type="amount"
+              placeholder="Token Amount"
+              name="amount"
+              value={amount}
+              onChange={onChange}
+            />
+          </Fragment>
+        );
+      case "erc721":
+        return (
+          <Fragment>
+            {erc()}
+            <input
+              className="grow-1"
+              type="id"
+              placeholder="Token ID"
+              name="id"
+              value={id}
+              onChange={onChange}
+            />
+          </Fragment>
+        );
+
+      default:
+        return "";
+    }
   };
 
   //Render
   return (
     <form onSubmit={onSubmit} className="my-2">
-      <input
-        type="text"
-        placeholder="Contract Address"
-        name="contractAdd"
-        value={contractAdd}
-        onChange={onChange}
-      />
-      <input
-        type="text"
-        placeholder="type"
-        name="type"
-        value={type}
-        onChange={onChange}
-      />
-      <input
-        // TODO conditional
-        type="id"
-        placeholder="id"
-        name="id"
-        value={id}
-        onChange={onChange}
-      />
+      <div className="flexRow">
+        <select type="text" name="type" value={type} onChange={onChange}>
+          <option value={"ens"}>ENS</option>
+          <option value={"erc20"}>ERC20</option>
+          <option value={"erc721"}>ERC721</option>
+        </select>
+        {form()}
+      </div>
       <input
         type="submit"
-        value="Add Trade"
+        value="Add Trade Item"
         className="btn btn-dark btn-block"
       />
     </form>
