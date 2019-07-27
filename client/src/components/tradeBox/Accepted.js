1ac9ae35896de7ba80fdfbfa55d457eca6c1fa1b
Flash <flash.jaq@gmail.com>
2019-07-27 22:20:44 +0200

    Styled Accepted

--- client/src/components/tradeBox/Accepted.js
+++ client/src/components/tradeBox/Accepted.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Accepted = () => {
-  return <div className="accepted">Accepted</div>;
+  return (
+    <div className="accepted">
+      <h3>Accepted</h3>
+    </div>
+  );
 };
 
 export default Accepted;
