b17d02211a1a3d38346b58f91224bdc7792749ff
Flash <flash.jaq@gmail.com>
2019-07-28 01:49:32 +0200

    Cleaned spinner

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,25 +1,30 @@
 import React from "react";
 import spinner from "./spinner.gif";
 
-const Spinner = ({ height }) => (
-  <img
-    src={spinner}
-    alt="Loading..."
-    style={
-      height
-        ? {
-            height: "7rem",
-            width: "7rem",
-            margin: "auto",
-            display: "block"
-          }
-        : {
-            width: "100%",
-            margin: "auto",
-            display: "block"
-          }
-    }
-  />
-);
+const Spinner = ({ size }) => {
+  const style = {
+    margin: "auto",
+    display: "block"
+  };
+
+  switch (size) {
+    case 5:
+      style.width = "5rem";
+      style.height = "5rem";
+      break;
+    case 7:
+      style.width = "7rem";
+      style.height = "7rem";
+      break;
+    case 10:
+      style.width = "10rem";
+      style.height = "10rem";
+      break;
+    default:
+      style.width = "100%";
+  }
+
+  return <img src={spinner} alt="Loading..." style={style} />;
+};
 
 export default Spinner;
