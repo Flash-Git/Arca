cebebae8f0ba7a14669d0841c082f22ba25a1da0
Flash <flash.jaq@gmail.com>
2019-07-28 03:34:52 +0200

    Refactored

--- client/src/context/user/UserState.js
+++ client/src/context/user/UserState.js
@@ -4,17 +4,28 @@ import UserContext from "./UserContext";
 import UserReducer from "./UserReducer";
 
 import {
-  SET_USER,
-  SET_TRADE_PARTNER,
   CLEAR_USER,
-  CLEAR_TRADE_PARTNER
+  CLEAR_TRADE_PARTNER,
+  SET_ADDRESS,
+  CLEAR_ADDRESS,
+  SET_BALANCE,
+  CLEAR_BALANCE,
+  SET_OWNED_TOKENS,
+  SET_REQUESTED_TOKENS,
+  ADD_REQUESTED_TOKEN,
+  SET_SETTINGS,
+  UPDATE_SETTINGS,
+  CLEAR_SETTINGS
 } from "../types";
 
 const UserState = props => {
   const initialState = {
     user: {
-      address: "",
-      ens: null,
+      addressObj: {
+        address: "",
+        ens: null
+      },
+
       balance: null,
       ownedTokens: {
         web3Loading: false,
@@ -32,8 +43,10 @@ const UserState = props => {
       }
     },
     tradePartner: {
-      address: "",
-      ens: null,
+      addressObj: {
+        address: "",
+        ens: null
+      },
       balance: null,
       ownedTokens: {
         web3Loading: false,
@@ -61,20 +74,6 @@ const UserState = props => {
    * Actions
    */
 
-  const setUser = user => {
-    dispatch({
-      type: SET_USER,
-      payload: user
-    });
-  };
-
-  const setTradePartner = tradePartner => {
-    dispatch({
-      type: SET_TRADE_PARTNER,
-      payload: tradePartner
-    });
-  };
-
   const clearUser = () => {
     dispatch({
       type: CLEAR_USER
@@ -87,16 +86,104 @@ const UserState = props => {
     });
   };
 
+  const getAddress = (user, addressObj) => {
+    //GET addressObj from web3state (browser)
+    dispatch({
+      type: SET_ADDRESS,
+      payload: { user, addressObj }
+    });
+  };
+
+  const clearAddress = user => {
+    dispatch({
+      type: CLEAR_ADDRESS,
+      payload: user
+    });
+  };
+
+  const getBalance = (user, balance) => {
+    //GET balance from db and web3
+    dispatch({
+      type: SET_BALANCE,
+      payload: { user, balance }
+    });
+  };
+
+  const clearBalance = user => {
+    dispatch({
+      type: CLEAR_BALANCE,
+      payload: user
+    });
+  };
+
+  const getOwnedTokens = user => {
+    //GET tokens from db and web3 (with map)
+    const ownedTokens = [];
+
+    dispatch({
+      type: SET_OWNED_TOKENS,
+      payload: { user, ownedTokens }
+    });
+  };
+
+  const getRequestedTokens = user => {
+    //GET tokens from db
+    const ownedTokens = [];
+
+    dispatch({
+      type: SET_REQUESTED_TOKENS,
+      payload: { user, ownedTokens }
+    });
+  };
+
+  const addRequestedToken = token => {
+    dispatch({
+      type: ADD_REQUESTED_TOKEN,
+      payload: token
+    });
+  };
+
+  const getSettings = () => {
+    //GET tokens from db
+    const settings = {};
+
+    dispatch({
+      type: SET_SETTINGS,
+      payload: settings
+    });
+  };
+
+  const updateSettings = settings => {
+    dispatch({
+      type: UPDATE_SETTINGS,
+      payload: settings
+    });
+  };
+
+  const clearSettings = () => {
+    dispatch({
+      type: CLEAR_SETTINGS
+    });
+  };
+
   return (
     <UserContext.Provider
       value={{
         user: state.user,
         tradePartner: state.tradePartner,
         settings: state.settings,
-        setUser,
-        setTradePartner,
         clearUser,
-        clearTradePartner
+        clearTradePartner,
+        getAddress,
+        clearAddress,
+        getBalance,
+        clearBalance,
+        getOwnedTokens,
+        getRequestedTokens,
+        addRequestedToken,
+        getSettings,
+        updateSettings,
+        clearSettings
       }}
     >
       {props.children}
