0baf3de7564a9c0088dae410990dd6d33f26e549
James Quinn <flash.jaq@gmail.com>
2019-07-29 00:34:14 +0200

    Added vars

--- client/src/components/tradeBox/itemTypes/Erc20.js
+++ client/src/components/tradeBox/itemTypes/Erc20.js
@@ -2,9 +2,13 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 const Erc20 = ({ item }) => {
+  const { contractAdd, amount } = item.data;
+
   return (
     <Fragment>
       <h3>ERC20</h3>
+      <span>contractAdd</span>
+      <span>amount</span>
     </Fragment>
   );
 };
--- client/src/components/tradeBox/itemTypes/Erc721.js
+++ client/src/components/tradeBox/itemTypes/Erc721.js
@@ -2,9 +2,13 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 const Erc721 = ({ item }) => {
+  const { contractAdd, id } = item.data;
+
   return (
     <Fragment>
       <h3>ERC721</h3>
+      <span>contractAdd</span>
+      <span>id</span>
     </Fragment>
   );
 };
