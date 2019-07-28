62f5dca35f69b278db6d8c81d46f5343738ac5ef
James Quinn <flash.jaq@gmail.com>
2019-07-29 00:19:04 +0200

    Added footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,68 +4,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
-    <footer id="section-footer" className="section" style={footerStyle}>
-      <ul style={ulStyle}>
-        <a style={linkStyle} href="https://etherscan.io/address/jaquinn.eth">
-          <FontAwesomeIcon style={iconStyle} icon={["fab", "ethereum"]} />
+    <footer className="footer">
+      <ul>
+        <a href="https://etherscan.io/address/jaquinn.eth">
+          <FontAwesomeIcon icon={["fab", "ethereum"]} />
           &nbsp;Donations
         </a>
-        <a style={linkStyle} href="mailto:arca@jaquinn.com">
-          <FontAwesomeIcon style={iconStyle} icon={["fa", "at"]} />
+        <a href="mailto:arca@jaquinn.com">
+          <FontAwesomeIcon icon={["fa", "at"]} />
           &nbsp;Inquiries
         </a>
-        <a
-          style={linkStyle}
-          href="https://blockstream.info/address/bc1qvn7u88k5qpq2m7g8mu8swh88v7q59msxa4w6vg"
-        >
-          <FontAwesomeIcon style={iconStyle} icon={["fab", "bitcoin"]} />
+        <a href="https://blockstream.info/address/bc1qvn7u88k5qpq2m7g8mu8swh88v7q59msxa4w6vg">
+          <FontAwesomeIcon icon={["fab", "bitcoin"]} />
           &nbsp;Donations
         </a>
-        <a style={linkStyle} href="https://twitter.com/EthArca">
-          <FontAwesomeIcon style={iconStyle} icon={["fab", "twitter"]} />
+        <a href="https://twitter.com/EthArca">
+          <FontAwesomeIcon icon={["fab", "twitter"]} />
           &nbsp;Twitter
         </a>
-        <a style={linkStyle} href="https://github.com/Flash-Git/Arca">
-          <FontAwesomeIcon style={iconStyle} icon={["fab", "github"]} />
+        <a href="https://github.com/Flash-Git/Arca">
+          <FontAwesomeIcon icon={["fab", "github"]} />
           &nbsp;Source
         </a>
       </ul>
     </footer>
   );
-};
-
-const footerStyle = {
-  display: "flex",
-  flexWrap: "wrap",
-  flexDirection: "row",
-  justifyContent: "center",
-  textAlign: "center",
-  //backgroundColor: ,
-  marginTop: "auto"
-};
-
-const ulStyle = {
-  display: "flex",
-  flexWrap: "wrap",
-  flexDirection: "row",
-  justifyContent: "center",
-  textAlign: "center",
-  maxWidth: "24rem",
-  listStyle: "none",
-  padding: "0",
-  margin: "0.5rem"
-};
-
-const linkStyle = {
-  textAlign: "left",
-  width: "6rem",
-  //color: colours.Primary,
-  textDecoration: "none",
-  margin: "0.5rem 0.75rem"
-};
-
-const iconStyle = {
-  width: "1rem"
 };
 
 export default Footer;
