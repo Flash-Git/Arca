5e4fa9621efba617871311f8e43f018560af551e
Flash <flash.jaq@gmail.com>
2019-07-28 18:05:17 +0200

    Added footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,44 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBoxOpen, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBoxOpen,
+  faInfoCircle,
+  faAt
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faTwitter,
+  faEthereum,
+  faBitcoin
+} from "@fortawesome/free-brands-svg-icons";
 
 import Alerts from "./components/layout/Alerts";
 import Navbar from "./components/layout/Navbar";
-import Sidebar from "./components/layout/Sidebar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
+import Footer from "./components/layout/Footer";
 
 import AppState from "./context/app/AppState";
 import AlertState from "./context/alert/AlertState";
 import TradeState from "./context/trade/TradeState";
 import UserState from "./context/user/UserState";
 import Web3State from "./context/web3/Web3State";
-import EnsState from "./context/ens/EnsState";
 
 import setAuthToken from "./utils/setAuthToken.js";
 
 import "./App.css";
 
-library.add(faBoxOpen, faInfoCircle);
+library.add(
+  faBoxOpen,
+  faInfoCircle,
+  faAt,
+  faGithub,
+  faTwitter,
+  faEthereum,
+  faBitcoin
+);
 
 localStorage.token && setAuthToken(localStorage.token);
 
@@ -32,22 +49,16 @@ const App = () => (
       <UserState>
         <AlertState>
           <Web3State>
-            <EnsState>
-              <Router>
-                <Fragment>
-                  <Navbar />
-                  <Sidebar />
-                  <div className="container">
-                    <Alerts />
-                    <Switch>
-                      <Route exact path="/" component={Home} />
-                      <Route exact path="/about" component={About} />
-                      <Route component={NotFound} />
-                    </Switch>
-                  </div>
-                </Fragment>
-              </Router>
-            </EnsState>
+            <Router>
+              <Navbar />
+              <Alerts />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+              <Footer />
+            </Router>
           </Web3State>
         </AlertState>
       </UserState>
