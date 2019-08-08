5ce0103b9592f863767468256a28e026894e3217
Flash <flash.jaq@gmail.com>
2019-08-08 17:14:59 +0200

    Added hidden sidebar

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Fragment, useState, useContext } from "react";
 
 import UserContext from "../../context/user/UserContext";
 import Web3Context from "../../context/web3/Web3Context";
@@ -12,6 +12,12 @@ const Sidebar = () => {
   const { nickname } = settings;
 
   const { connected, loading } = web3Context;
+
+  const [hidden, setHidden] = useState(true);
+
+  const onClick = e => {
+    setHidden(!hidden);
+  };
 
   const tokens = () => (
     <ul>
@@ -34,11 +40,24 @@ const Sidebar = () => {
     </ul>
   );
 
-  return (
-    <div className="sidebar">
-      <h2>{nickname && nickname}</h2>
-      <div>{connected ? <p>Not Connected</p> : tokens()}</div>
+  return hidden ? (
+    <div style={{ float: "right", margin: "1rem" }}>
+      <button onClick={onClick} className="btn btn-dark">
+        Show
+      </button>
     </div>
+  ) : (
+    <Fragment>
+      <div style={{ float: "right", margin: "1rem" }}>
+        <button onClick={onClick} className="btn btn-dark">
+          Hide
+        </button>
+      </div>
+      <div className="sidebar">
+        <h2>{nickname && nickname}</h2>
+        <div>{connected ? <p>Not Connected</p> : tokens()}</div>
+      </div>
+    </Fragment>
   );
 };
 
