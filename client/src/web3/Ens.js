c3411701de2b7f7d4e243ae278714d26269999de
Flash <flash.jaq@gmail.com>
2019-08-12 22:43:29 +0200

    Removed ENS file

--- client/src/web3/Ens.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import Web3Context from "../context/web3/Web3Context";
-
-export const getAddress = name => {
-  const web3Context = useContext(Web3Context);
-  const { ens } = web3Context;
-};
