45a05d0fab0545d29e449a9fe9b550a24042bef9
J Quinn <git@jaquinn.com>
2019-09-08 23:40:15 +0200

    Added erc20 loading

--- client/src/components/loading/Loader.js
+++ client/src/components/loading/Loader.js
@@ -1,17 +1,86 @@
 import React, { useContext, useEffect, useState } from "react";
 
 import Web3Context from "../../context/web3/Web3Context";
+import UserContext from "../../context/user/UserContext";
 
 const Loader = () => {
   const web3Context = useContext(Web3Context);
   const { web3, ArcaCalls, ErcCalls } = web3Context;
 
-  const [loadArray, setloadArray] = useState([]);
+  const userContext = useContext(UserContext);
+  const userAdd = userContext.user.addressObj.address;
+  const partnerAdd = userContext.tradePartner.addressObj.address;
 
-  useEffect(() => {
+  const initialState = {
+    loaded: false,
+    erc20Count: null,
+    erc721Count: null,
+    erc20s: [],
+    erc721s: []
+  };
+
+  const [user, setUser] = useState(initialState);
+  const [partner, setPartner] = useState(initialState);
+
+  const getErc20s = (_erc20Count, _add1, _add2) => {
     if (web3 === null) return;
-    //Fill trade box with web3 calls
-  }, [web3]);
+    const erc20Promises = [];
+
+    for (let i = 0; i < _erc20Count; i++) {
+      erc20Promises.push(ArcaCalls("getOfferErc20", [_add1, _add2, i]));
+    }
+
+    const offers = [];
+
+    Promise.all(erc20Promises).then(erc20s => {
+      erc20s.map(erc20 => {
+        let offer = {
+          id: "0-" + i,
+          network: { slot: i },
+          data: { type: "erc20", contractAdd: "", amount: "" }
+        };
+        [offer.data.contractAdd, offer.data.amount] = [
+          erc20[0].toString(), //contractAdd
+          erc20[1].toString() //amount
+        ];
+        offers.push(offer);
+      });
+      return offers;
+    });
+
+    //Hook based async
+    useEffect(() => {
+      if (web3 === null) {
+        setUser(initialState);
+        setPartner(initialState);
+        return;
+      }
+
+      ArcaCalls("getErc20Count", [userAdd, partnerAdd]).then(erc20Count => {
+        setUser(...user, +erc20Count);
+        getErc20s(+erc20Count, userAdd, partnerAdd).then(erc20s =>
+          setUser(erc20s)
+        );
+      });
+
+      ArcaCalls("getErc721Count", [userAdd, partnerAdd]).then(erc721Count => {
+        setUser(...user, +erc721Count);
+        getErc721s(+erc721Count, userAdd, partnerAdd);
+      });
+
+      ArcaCalls("getErc20Count", [partnerAdd, userAdd]).then(erc20Count => {
+        setUser(...partner, +erc20Count);
+        getErc20s(+erc20Count, partnerAdd, userAdd).then(erc20s =>
+          setPartner(erc20s)
+        );
+      });
+
+      ArcaCalls("getErc721Count", [partnerAdd, userAdd]).then(erc721Count => {
+        setPartner(...partner, erc721Count);
+        getErc721s(+erc721Count, partnerAdd, userAdd);
+      });
+    }, [userAdd, partnerAdd]);
+  };
 
   return <button className="btn">Reload box</button>;
 };
