f9fa5f954d9bfc7d4754f68c4c2d82ae1e186c5e
J Quinn <git@jaquinn.com>
2019-09-09 18:36:14 +0200

    Added loader

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react";
 
 import Sidebar from "../layout/Sidebar";
 import PreTradeForm from "../layout/PreTradeForm";
+import Loader from "../loading/Loader";
 import Box from "../tradeBox/Box";
 
 import AppContext from "./../../context/app/AppContext";
@@ -23,6 +24,7 @@ const Home = props => {
       <Sidebar />
       <div className="container">
         <PreTradeForm />
+        <Loader />
         {userBox === 0 ? <Box isUser={true} /> : <Box />}
         {userBox === 1 ? <Box isUser={true} /> : <Box />}
       </div>
