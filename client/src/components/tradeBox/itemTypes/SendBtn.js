915d966721edd4d648c8fd475fac08a20919895a
J Quinn <git@jaquinn.com>
2019-09-09 16:22:14 +0200

    Added userCheck

--- client/src/components/tradeBox/itemTypes/SendBtn.js
+++ client/src/components/tradeBox/itemTypes/SendBtn.js
@@ -6,7 +6,7 @@ import { SENT, UNSENT } from "../../../context/sentStatus";
 import Web3Context from "../../../context/web3/Web3Context";
 import TradeContext from "../../../context/trade/TradeContext";
 
-const SendBtn = ({ status, txData }) => {
+const SendBtn = ({ status, txData, isUser }) => {
   //txData{ method:"pushOfferErc20" }
   //erc - allow
   //arca - send
@@ -20,34 +20,48 @@ const SendBtn = ({ status, txData }) => {
   const [content, setContent] = useState("Loading");
 
   useEffect(() => {
+    if (!isUser) {
+      setContent("Sent");
+      return;
+    }
+
     switch (status) {
       case SENT:
         setContent("Cancel");
-        break;
+        return;
       case UNSENT:
         setContent("Send");
-        break;
+        return;
       default:
         setContent("Error");
-        break;
+        return;
     }
   }, [status]);
 
   const onClick = e => {
     ArcaSends(txData.method, txData.params);
-    modifyTradeItemStatus(status);
+    //modifyTradeItemStatus(status);
   };
 
-  return (
-    <button className="btn btn-sm w-5" onClick={onClick}>
-      {content}
-    </button>
-  );
+  const button = () => {
+    if (isUser) {
+      return (
+        <button className="btn btn-sm w-5" onClick={onClick}>
+          {content}
+        </button>
+      );
+    } else {
+      return <div className="btn btn-sm w-5">{content}</div>;
+    }
+  };
+
+  return button();
 };
 
 SendBtn.propTypes = {
   status: PropTypes.string.isRequired,
-  txData: PropTypes.object.isRequired
+  txData: PropTypes.object,
+  isUser: PropTypes.bool.isRequired
 };
 
 export default SendBtn;
