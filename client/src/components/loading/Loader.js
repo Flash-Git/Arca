9b3bd041697a3e111ae91cd6ffe7ea34ccf28433
J Quinn <git@jaquinn.com>
2019-09-10 06:48:43 +0200

    Added promise handling

--- client/src/components/loading/Loader.js
+++ client/src/components/loading/Loader.js
@@ -5,7 +5,7 @@ import UserContext from "../../context/user/UserContext";
 
 const Loader = () => {
   const web3Context = useContext(Web3Context);
-  const { web3, ArcaCalls, ErcCalls } = web3Context;
+  const { connected, ArcaCalls, ErcCalls } = web3Context;
 
   const userContext = useContext(UserContext);
   const userAdd = userContext.user.addressObj.address;
@@ -24,7 +24,7 @@ const Loader = () => {
   const [partner, setPartner] = useState(initialState);
 
   const getErc20s = (_erc20Count, _add1, _add2) => {
-    if (web3 === null) return;
+    if (!connected) return;
     const erc20Promises = [];
 
     for (let i = 0; i < _erc20Count; i++) {
@@ -33,25 +33,31 @@ const Loader = () => {
 
     const offers = [];
 
-    Promise.all(erc20Promises).then(erc20s => {
-      erc20s.map((erc20, i) => {
-        let offer = {
-          id: "0-" + i,
-          network: { slot: i },
-          data: { type: "erc20", contractAdd: "", amount: "" }
-        };
-        [offer.data.contractAdd, offer.data.amount] = [
-          erc20[0].toString(), //contractAdd
-          erc20[1].toString() //amount
-        ];
-        offers.push(offer);
-      });
-      return offers;
-    });
+    return new Promise((resolve, reject) =>
+      Promise.all(erc20Promises)
+        .then(erc20s => {
+          erc20s.map((erc20, i) => {
+            let offer = {
+              id: "0-" + i,
+              network: { slot: i },
+              data: { type: "erc20", contractAdd: "", amount: "" }
+            };
+            [offer.data.contractAdd, offer.data.amount] = [
+              erc20[0].toString(), //contractAdd
+              erc20[1].toString() //amount
+            ];
+            offers.push(offer);
+          });
+          resolve(offers);
+        })
+        .catch(e => {
+          reject(e);
+        })
+    );
   };
 
   const getErc721s = (_erc721Count, _add1, _add2) => {
-    if (web3 === null) return;
+    if (!connected) return;
     const erc721Promises = [];
 
     for (let i = 0; i < _erc721Count; i++) {
@@ -60,25 +66,31 @@ const Loader = () => {
 
     const offers = [];
 
-    Promise.all(erc721Promises).then(erc721s => {
-      erc721s.map((erc721, i) => {
-        let offer = {
-          id: "1-" + i,
-          network: { slot: i },
-          data: { type: "erc721", contractAdd: "", id: "" }
-        };
-        [offer.data.contractAdd, offer.data.id] = [
-          erc721[0].toString(), //contractAdd
-          erc721[1].toString() //id
-        ];
-        offers.push(offer);
-      });
-      return offers;
-    });
+    return new Promise((resolve, reject) =>
+      Promise.all(erc721Promises)
+        .then(erc721s => {
+          erc721s.map((erc721, i) => {
+            let offer = {
+              id: "1-" + i,
+              network: { slot: i },
+              data: { type: "erc721", contractAdd: "", id: "" }
+            };
+            [offer.data.contractAdd, offer.data.id] = [
+              erc721[0].toString(), //contractAdd
+              erc721[1].toString() //id
+            ];
+            offers.push(offer);
+          });
+          resolve(offers);
+        })
+        .catch(e => {
+          reject(e);
+        })
+    );
   };
 
   const load = () => {
-    if (web3 === null) {
+    if (!connected) {
       setUser(initialState);
       setPartner(initialState);
       return;
@@ -92,33 +104,43 @@ const Loader = () => {
     ArcaCalls("getErc20Count", [userAdd, partnerAdd]).then(erc20Count => {
       erc20Count = +erc20Count;
       setUser({ ...user, erc20Count });
-      getErc20s(erc20Count, userAdd, partnerAdd).then(erc20s =>
-        setUser({ ...user, erc20s, loadedErc20s: true })
-      );
+      getErc20s(erc20Count, userAdd, partnerAdd)
+        .then(erc20s => setUser({ ...user, erc20s, loadedErc20s: true }))
+        .catch(e => {
+          console.log(e);
+        });
     });
 
     ArcaCalls("getErc721Count", [userAdd, partnerAdd]).then(erc721Count => {
       erc721Count = +erc721Count;
       setUser({ ...user, erc721Count });
-      getErc721s(erc721Count, userAdd, partnerAdd).then(erc721s =>
-        setUser({ ...user, erc721s, loadedErc721s: true })
-      );
+      getErc721s(erc721Count, userAdd, partnerAdd)
+        .then(erc721s => setUser({ ...user, erc721s, loadedErc721s: true }))
+        .catch(e => {
+          console.log(e);
+        });
     });
 
     ArcaCalls("getErc20Count", [partnerAdd, userAdd]).then(erc20Count => {
       erc20Count = +erc20Count;
       setPartner({ ...partner, erc20Count });
-      getErc20s(erc20Count, partnerAdd, userAdd).then(erc20s =>
-        setPartner({ ...partner, erc20s, loadedErc20s: true })
-      );
+      getErc20s(erc20Count, partnerAdd, userAdd)
+        .then(erc20s => setPartner({ ...partner, erc20s, loadedErc20s: true }))
+        .catch(e => {
+          console.log(e);
+        });
     });
 
     ArcaCalls("getErc721Count", [partnerAdd, userAdd]).then(erc721Count => {
       erc721Count = +erc721Count;
-      setPartner(...partner, erc721Count);
-      getErc721s(erc721Count, partnerAdd, userAdd).then(erc721s =>
-        setPartner({ ...partner, erc721s, loadedErc721s: true })
-      );
+      setPartner({ ...partner, erc721Count });
+      getErc721s(erc721Count, partnerAdd, userAdd)
+        .then(erc721s =>
+          setPartner({ ...partner, erc721s, loadedErc721s: true })
+        )
+        .catch(e => {
+          console.log(e);
+        });
     });
   };
 
@@ -139,7 +161,11 @@ const Loader = () => {
     //seterc20s on trade context
   }, [partner.erc20s, partner.erc721s]);
 
-  //return <button className="btn btn-dark" onClick={load}>Reload</button>;
+  return (
+    <button className="btn btn-dark" onClick={load}>
+      Reload
+    </button>
+  );
   return null;
 };
 
