cb8e114a0c4065fc7ceb71d0af18baf046dcd7a2
J Quinn <git@jaquinn.com>
2020-11-29 01:25:20 +0000

    Added types to loading

--- client/src/components/loading/Loader.tsx
+++ client/src/components/loading/Loader.tsx
@@ -1,18 +1,24 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 
 import Web3Context from "../../context/web3/Web3Context";
 import UserContext from "../../context/user/UserContext";
 import TradeContext from "../../context/trade/TradeContext";
 
-const Loader = () => {
-  const web3Context = useContext(Web3Context);
-  const { connected, ArcaCalls, ErcCalls } = web3Context;
+import {
+  TradeContext as ITradeContext,
+  Web3Context as IWeb3Context,
+  UserContext as IUserContext
+} from "context";
 
-  const userContext = useContext(UserContext);
+const Loader: FC = () => {
+  const web3Context: IWeb3Context = useContext(Web3Context);
+  const { connected, arcaCalls: ArcaCalls, ercCalls: ErcCalls } = web3Context;
+
+  const userContext: IUserContext = useContext(UserContext);
   const userAdd = userContext.user.addressObj.address;
   const partnerAdd = userContext.tradePartner.addressObj.address;
 
-  const tradeContext = useContext(TradeContext);
+  const tradeContext: ITradeContext = useContext(TradeContext);
   const {
     setUserAccepted,
     setPartnerAccepted,
@@ -27,8 +33,8 @@ const Loader = () => {
   const [user, setUser] = useState(initialState);
   const [partner, setPartner] = useState(initialState);
 
-  const getAccepted = (_add1, _add2) => {
-    const nonces = [];
+  const getAccepted = (_add1: string, _add2: string) => {
+    const nonces: Promise<string>[] = [];
     nonces.push(ArcaCalls("getNonce", [userAdd, partnerAdd]));
     nonces.push(ArcaCalls("getPartnerNonce", [userAdd, partnerAdd]));
     return new Promise((resolve, reject) =>
@@ -41,15 +47,17 @@ const Loader = () => {
     );
   };
 
-  const getErc20s = (_erc20Count, _add1, _add2) => {
+  const getErc20s = (_erc20Count: number, _add1: string, _add2: string) => {
     if (!connected) return;
-    const erc20Promises = [];
+    const erc20Promises: Promise<string>[] = [];
 
     for (let i = 0; i < _erc20Count; i++) {
-      erc20Promises.push(ArcaCalls("getOfferErc20", [_add1, _add2, i]));
+      erc20Promises.push(
+        ArcaCalls("getOfferErc20", [_add1, _add2, i.toString()])
+      );
     }
 
-    const offers = [];
+    const offers: any[] = [];
 
     return new Promise((resolve, reject) =>
       Promise.all(erc20Promises)
@@ -72,15 +80,17 @@ const Loader = () => {
     );
   };
 
-  const getErc721s = (_erc721Count, _add1, _add2) => {
+  const getErc721s = (_erc721Count: number, _add1: string, _add2: string) => {
     if (!connected) return;
-    const erc721Promises = [];
+    const erc721Promises: any[] = [];
 
     for (let i = 0; i < _erc721Count; i++) {
-      erc721Promises.push(ArcaCalls("getOfferErc721", [_add1, _add2, i]));
+      erc721Promises.push(
+        ArcaCalls("getOfferErc721", [_add1, _add2, i.toString()])
+      );
     }
 
-    const offers = [];
+    const offers: any[] = [];
 
     return new Promise((resolve, reject) =>
       Promise.all(erc721Promises)
@@ -131,9 +141,9 @@ const Loader = () => {
     userPromises.push(
       new Promise((resolve, reject) =>
         ArcaCalls("getErc20Count", [userAdd, partnerAdd]).then(erc20Count => {
-          getErc20s(+erc20Count, userAdd, partnerAdd)
-            .then(erc20s => resolve(erc20s))
-            .catch(e => reject(e));
+          const erc20s = getErc20s(+erc20Count, userAdd, partnerAdd);
+          if (erc20s)
+            erc20s.then(erc20s => resolve(erc20s)).catch(e => reject(e));
         })
       )
     );
@@ -141,9 +151,9 @@ const Loader = () => {
     userPromises.push(
       new Promise((resolve, reject) =>
         ArcaCalls("getErc721Count", [userAdd, partnerAdd]).then(erc721Count => {
-          getErc721s(+erc721Count, userAdd, partnerAdd)
-            .then(erc721s => resolve(erc721s))
-            .catch(e => reject(e));
+          const erc721s = getErc721s(+erc721Count, userAdd, partnerAdd);
+          if (erc721s)
+            erc721s.then(erc721s => resolve(erc721s)).catch(e => reject(e));
         })
       )
     );
@@ -159,9 +169,9 @@ const Loader = () => {
     partnerPromises.push(
       new Promise((resolve, reject) =>
         ArcaCalls("getErc20Count", [partnerAdd, userAdd]).then(erc20Count => {
-          getErc20s(+erc20Count, partnerAdd, userAdd)
-            .then(erc20s => resolve(erc20s))
-            .catch(e => reject(e));
+          const erc20s = getErc20s(+erc20Count, partnerAdd, userAdd);
+          if (erc20s)
+            erc20s.then(erc20s => resolve(erc20s)).catch(e => reject(e));
         })
       )
     );
@@ -169,16 +179,16 @@ const Loader = () => {
     partnerPromises.push(
       new Promise((resolve, reject) =>
         ArcaCalls("getErc721Count", [partnerAdd, userAdd]).then(erc721Count => {
-          getErc721s(+erc721Count, partnerAdd, userAdd)
-            .then(erc721s => resolve(erc721s))
-            .catch(e => reject(e));
+          const erc721s = getErc721s(+erc721Count, partnerAdd, userAdd);
+          if (erc721s)
+            erc721s.then(erc721s => resolve(erc721s)).catch(e => reject(e));
         })
       )
     );
 
     Promise.all(userPromises)
-      .then(res => {
-        setUserAccepted([res[0]]);
+      .then((res: any[]) => {
+        setUserAccepted(res[0]);
         setUserItems([...res[1], ...res[2]]);
         setUser({ loading: false });
       })
@@ -187,8 +197,8 @@ const Loader = () => {
         setUser({ loading: false });
       });
     Promise.all(partnerPromises)
-      .then(res => {
-        setPartnerAccepted([res[0]]);
+      .then((res: any[]) => {
+        setPartnerAccepted(res[0]);
         setPartnerItems([...res[1], ...res[2]]);
         setUser({ loading: false });
       })
--- client/src/components/loading/enable.tsx
+++ client/src/components/loading/enable.tsx
@@ -1,14 +1,19 @@
+interface Window {
+  ethereum: any;
+}
+declare var window: Window;
+
 const enable = () => {
-  if (window.ethereum === null) return false;
+  if (!window.ethereum) return false;
   return new Promise((resolve, reject) => {
     window.ethereum
       .enable()
-      .then(addresses => {
+      .then((addresses: string[]) => {
         console.log("Enabled");
         window.ethereum.autoRefreshOnNetworkChange = false; //TODO handle in state
         resolve(true);
       })
-      .catch(e => {
+      .catch((e: any) => {
         resolve(false);
       });
   });
