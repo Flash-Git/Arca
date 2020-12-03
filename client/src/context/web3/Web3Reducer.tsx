2e40345655b38c97b1fbe2a135492660fc4e74f2
J Quinn <git@jaquinn.com>
2020-12-03 02:58:09 +0000

    Set up new Web3Reducer

--- client/src/context/web3/Web3Reducer.tsx
+++ client/src/context/web3/Web3Reducer.tsx
@@ -1,114 +1,31 @@
 import {
-  CONNECT,
-  CONNECT_WEB3,
-  CONNECT_ENS,
-  UPDATE_NETWORK,
-  SET_ARCA,
-  ADD_ERC,
-  DISCONNECT
+  ADD_PROVIDER,
+  ADD_SIGNER,
+  SET_ARCA_ADDRESS,
+  SET_ARCA_CONTRACT,
+  SET_ERC20_ADDRESSES,
+  SET_ERC721_ADDRESSES
 } from "../types";
 
 import { Action, Web3State } from "context";
 
-const Web3Reducer = (state: Web3State, action: Action): Web3State => {
-  switch (action.type) {
-    case CONNECT:
-      return {
-        ...state,
-        connected: true
-      };
-    case CONNECT_WEB3:
-      return {
-        ...state,
-        web3: action.payload
-      };
-    case CONNECT_ENS:
-      return {
-        ...state,
-        ens: action.payload
-      };
-    case UPDATE_NETWORK:
-      switch (action.payload) {
-        case 1:
-          return {
-            ...state,
-            network: action.payload,
-            ...state.main
-          };
-        case 4:
-          return {
-            ...state,
-            network: action.payload,
-            ...state.rinkeby
-          };
-        case 5:
-          return {
-            ...state,
-            network: action.payload,
-            ...state.goerli
-          };
-        default:
-          return state;
-      }
-    case SET_ARCA:
-      switch (state.network) {
-        case 1:
-          return {
-            ...state,
-            main: { ...state.main, arca: action.payload }
-          };
-        case 4:
-          return {
-            ...state,
-            rinkeby: { ...state.main, arca: action.payload }
-          };
-        case 5:
-          return {
-            ...state,
-            goerli: { ...state.main, arca: action.payload }
-          };
-        default:
-          return state;
-      }
-    case ADD_ERC:
-      switch (state.network) {
-        case 1:
-          return {
-            ...state,
-            main: {
-              ...state.main,
-              ercs: [...state.main.ercs, action.payload]
-            }
-          };
-        case 4:
-          return {
-            ...state,
-            rinkeby: {
-              ...state.main,
-              ercs: [...state.rinkeby.ercs, action.payload]
-            }
-          };
-        case 5:
-          return {
-            ...state,
-            goerli: {
-              ...state.main,
-              ercs: [...state.goerli.ercs, action.payload]
-            }
-          };
-        default:
-          return state;
-      }
-    case DISCONNECT:
-      return {
-        ...state,
-        web3: null,
-        ens: null,
-        network: null,
-        address: "",
-        arca: null,
-        ercs: []
-      };
+const Web3Reducer = (
+  state: Web3State,
+  { payload, type }: Action
+): Web3State => {
+  switch (type) {
+    case ADD_PROVIDER:
+      return { ...state, providers: [...state.providers, payload] };
+    case ADD_SIGNER:
+      return { ...state, signers: [...state.signers, payload] };
+    case SET_ARCA_ADDRESS:
+      return { ...state, arcaAddress: payload };
+    case SET_ARCA_CONTRACT:
+      return { ...state, arcaContract: payload };
+    case SET_ERC20_ADDRESSES:
+      return { ...state, erc20Addresses: payload };
+    case SET_ERC721_ADDRESSES:
+      return { ...state, erc721Addresses: payload };
     default:
       return state;
   }
