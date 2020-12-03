4cf369201e0d29fbfb160367c1fff4e3e5ee06e3
J Quinn <git@jaquinn.com>
2020-12-03 05:30:54 +0000

    Filled UserState actions

--- client/src/context/user/UserReducer.tsx
+++ client/src/context/user/UserReducer.tsx
@@ -1,4 +1,4 @@
-import {} from "../types";
+import { SET_ADDRESS, SET_BALANCE, SET_ERC20S, SET_ERC721S } from "../types";
 
 import { Action, UserState } from "context";
 
@@ -7,6 +7,14 @@ const UserReducer = (
   { payload, type }: Action
 ): UserState => {
   switch (type) {
+    case SET_ADDRESS:
+      return { ...state, address: payload };
+    case SET_BALANCE:
+      return { ...state, balance: payload };
+    case SET_ERC20S:
+      return { ...state, erc20s: payload };
+    case SET_ERC721S:
+      return { ...state, erc721s: payload };
     default:
       return state;
   }
--- client/src/context/user/UserState.tsx
+++ client/src/context/user/UserState.tsx
@@ -1,13 +1,27 @@
-import React, { FC, useReducer } from "react";
+import { FC, useContext, useReducer } from "react";
 
 import UserContext from "./UserContext";
 import UserReducer from "./UserReducer";
 
-import { UserState as IUserState } from "context";
+import AlertContext from "../alert/AlertContext";
 
-import {} from "../types";
+import {
+  AlertContext as IAlertContext,
+  UserState as IUserState,
+  LoadAddress,
+  LoadBalance,
+  LoadErcs,
+  Erc20,
+  Erc721
+} from "context";
+
+import { SET_ADDRESS, SET_BALANCE, SET_ERC20S, SET_ERC721S } from "../types";
+import { ercCall } from "../../web3/calls/ercCalls";
 
 const UserState: FC = props => {
+  const alertContext: IAlertContext = useContext(AlertContext);
+  const { addAlert } = alertContext;
+
   const initialState: IUserState = {
     address: "",
     balance: "",
@@ -22,6 +36,102 @@ const UserState: FC = props => {
    * Actions
    */
 
+  const loadAddress: LoadAddress = async signer => {
+    try {
+      const address = await signer.getAddress();
+
+      dispatch({
+        type: SET_ADDRESS,
+        payload: address
+      });
+    } catch (e) {
+      addAlert(e, "danger");
+    }
+  };
+
+  const loadBalance: LoadBalance = async signer => {
+    try {
+      const balance = await signer.getBalance();
+
+      dispatch({
+        type: SET_BALANCE,
+        payload: balance
+      });
+    } catch (e) {
+      addAlert(e, "danger");
+    }
+  };
+
+  const loadErc20s: LoadErcs = async (signer, erc20Addresses, arcaAddress) => {
+    // Check list of erc20Addresses and get store the user's balance'
+
+    try {
+      const balances: string[] = await Promise.all(
+        erc20Addresses.map(erc20Address =>
+          ercCall(
+            signer,
+            "balanceOf",
+            state.address,
+            arcaAddress,
+            erc20Address,
+            "erc20"
+          )
+        )
+      );
+
+      const erc20s: Erc20[] = balances.map((balance, id) => ({
+        address: erc20Addresses[id],
+        balance,
+        value: "unknown"
+      }));
+
+      dispatch({
+        type: SET_ERC20S,
+        payload: erc20s
+      });
+    } catch (e) {
+      addAlert(e, "danger");
+    }
+  };
+
+  const loadErc721s: LoadErcs = async (
+    signer,
+    erc721Addresses,
+    arcaAddress
+  ) => {
+    // Check list of erc20Addresses and get store the user's balance'
+
+    try {
+      const balances: string[] = await Promise.all(
+        erc721Addresses.map(erc721Addresses =>
+          ercCall(
+            signer,
+            "balanceOf",
+            state.address,
+            arcaAddress,
+            erc721Addresses,
+            "erc721"
+          )
+        )
+      );
+
+      const erc721s: Erc721[] = balances.map((balance, id) => ({
+        address: erc721Addresses[id],
+        id: balance,
+        value: "unknown"
+      }));
+
+      dispatch({
+        type: SET_ERC721S,
+        payload: erc721s
+      });
+    } catch (e) {
+      addAlert(e, "danger");
+    }
+  };
+
+  // Trade Items
+
   return (
     <UserContext.Provider
       value={{
@@ -29,7 +139,11 @@ const UserState: FC = props => {
         balance: state.balance,
         erc20s: state.erc20s,
         erc721s: state.erc721s,
-        tradeItems: state.tradeItems
+        tradeItems: state.tradeItems,
+        loadAddress,
+        loadBalance,
+        loadErc20s,
+        loadErc721s
       }}
     >
       {props.children}
