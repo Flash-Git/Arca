8f7759acbb1b357e51c1ca7e1580c969daa282c8
J Quinn <git@jaquinn.com>
2020-12-07 10:52:20 +0000

    Cleaned up Accepted and added alert

--- client/src/components/tradeBox/Accepted.tsx
+++ client/src/components/tradeBox/Accepted.tsx
@@ -1,7 +1,8 @@
-import React, { FC, Fragment, useContext } from "react";
+import { FC, Fragment, useContext, useEffect } from "react";
 
 import { arcaCall } from "../../web3/calls/arcaCalls";
 
+import AlertContext from "../../context/alert/AlertContext";
 import PartnerContext from "../../context/partner/PartnerContext";
 import UserContext from "../../context/user/UserContext";
 import Web3Context from "../../context/web3/Web3Context";
@@ -9,6 +10,7 @@ import Web3Context from "../../context/web3/Web3Context";
 import Spinner from "../layout/Spinner";
 
 import {
+  AlertContext as IAlertContext,
   UserContext as IUserContext,
   PartnerContext as IPartnerContext,
   Web3Context as IWeb3Context
@@ -19,44 +21,67 @@ type Props = {
 };
 
 const Accepted: FC<Props> = ({ isUser }) => {
+  const alertContext: IAlertContext = useContext(AlertContext);
   const userContext: IUserContext = useContext(UserContext);
   const partnerContext: IPartnerContext = useContext(PartnerContext);
   const web3Context: IWeb3Context = useContext(Web3Context);
 
-  const accepted = isUser ? userContext.accepted : partnerContext.accepted;
+  const { addAlert } = alertContext;
 
-  const { address: userAdd, toggleAccepted } = userContext;
-  const { address: partnerAdd } = partnerContext;
+  const { toggleAccepted } = userContext;
+  const { accepted, loadAccepted, address: add1 } = isUser
+    ? userContext
+    : partnerContext;
+  const { address: add2 } = !isUser ? userContext : partnerContext;
 
   const { arcaContract } = web3Context;
 
-  const bord = accepted ? "bord-hori-green" : "bord-hori-red";
-  const acceptMsg = accepted ? "Accepted" : "Not Accepted";
+  useEffect(() => {
+    if (arcaContract === null || add1 === "" || add2 === "") return;
+
+    loadAccepted(arcaContract, [add2, add1]);
+  }, [arcaContract, add1, add2]);
 
   const onClick = async () => {
-    if (accepted === null || arcaContract === null) return;
+    if (
+      accepted === null ||
+      arcaContract === null ||
+      add1 === "" ||
+      add2 === ""
+    )
+      return;
 
-    const partnerNonce = await arcaCall(arcaContract, "getPartnerNonce", [
-      userAdd,
-      partnerAdd
-    ]);
+    try {
+      const partnerNonce = await arcaCall(arcaContract, "getPartnerNonce", [
+        add1,
+        add2
+      ]);
 
-    const params = [partnerAdd, partnerNonce];
-
-    toggleAccepted(
-      arcaContract,
-      accepted ? "unacceptTrade" : "acceptTrade",
-      params
-    );
+      toggleAccepted(arcaContract, accepted ? "unacceptTrade" : "acceptTrade", [
+        add2,
+        partnerNonce
+      ]);
+    } catch (e) {
+      console.log(e);
+      addAlert(e.toString(), "danger");
+    }
   };
 
   return (
     <div className="accepted text-center">
       {accepted === null ? (
-        <Spinner />
+        <div style={{ margin: "0.1rem" }}>
+          <Spinner />
+        </div>
       ) : (
         <Fragment>
-          <h3 className={bord + " width100 py"}>{acceptMsg}</h3>
+          <h3
+            className={`${
+              accepted ? "bord-hori-green" : "bord-hori-red"
+            } width100 py`}
+          >
+            {accepted ? "Accepted" : "Not Accepted"}
+          </h3>
           {isUser && (
             <button onClick={onClick} className="btn btn-sm btn-dark my">
               {accepted ? "Reject" : "Accept"}
